fa7f22e7feba9435be6239471cda3f9c57c74d5f
declan0873 <declan.c@hotmail.com>
2019-11-23 14:50:22 +0000

    Initial Commit

---
+++ unit-testing-snippet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  unit-testing-snippet
+//
+//  Created by Declan on 23/11/2019.
+//  Copyright © 2019 Declan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
