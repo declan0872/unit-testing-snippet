dc9d4b131a0187558588fad72b511500fa4c6a40
declan0873 <declan.c@hotmail.com>
2019-12-08 13:32:28 +0000

    first commit

---
+++ unit-testing-snippet/Student.swift
@@ -0,0 +1,13 @@
+//
+//  Student.swift
+//  unit-testing-snippet
+//
+//  Created by Declan on 23/11/2019.
+//  Copyright © 2019 Declan. All rights reserved.
+//
+
+import UIKit
+
+class Student: NSObject {
+
+}
---
+++ unit-testing-snippetTests/StudentTests.swift
@@ -0,0 +1,9 @@
+//
+//  StudentTests.swift
+//  unit-testing-snippetTests
+//
+//  Created by Declan on 03/12/2019.
+//  Copyright © 2019 Declan. All rights reserved.
+//
+
+import Foundation
